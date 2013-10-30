1ac9a55db38817b11359503f90fabc0ad113a515
fredericmarchand <fredericmarchand@cmail.carleton.ca>
2013-10-29 22:43:36 -0400

    up to req15

--- src/fixtures/MyFixture.java
+++ src/fixtures/MyFixture.java
@@ -20,6 +20,15 @@ public class MyFixture extends ColumnFixture {
 	
 	public int farmX, farmY;
 	
+	public int animalType, animalsBeforeHarvest;
+	
+	public boolean orthAdj;
+	
+	public int playerWood;
+	public int playerReed;
+	
+	public int stableCost;
+	
 	// random test to try fitnesse
 	public int sum() {
 		int result;
@@ -139,5 +148,205 @@ public class MyFixture extends ColumnFixture {
 		ac.b[farmX][farmY].doClick();
 		return ac.farm[0][farmX][farmY].getType();
 	}
+	
+	// r-008
+	public int animalsAfterHarvest() {
+		AgricolaController ac = new AgricolaController(1);
+		ac.players[0].space = 20;
+		
+		if (animalType == 0)
+			ac.players[0].sheep = animalsBeforeHarvest;
+		else if (animalType == 1)
+			ac.players[0].boar = animalsBeforeHarvest;
+		else if (animalType == 2)
+			ac.players[0].cattle = animalsBeforeHarvest;
+		
+		ac.turn = 4;
+		ac.players[0].activefam = 0;
+		ac.players[0].food = 6;
+		ac.update(false);
+		
+		if (animalType == 0)
+			return ac.players[0].sheep;
+		else if (animalType == 1)
+			return ac.players[0].boar;
+		else if (animalType == 2)
+			return ac.players[0].cattle;
+		return 0;
+	}
 
+	// r-009
+	public char playerBuildPastures() {
+		AgricolaController ac = new AgricolaController(1);
+		ac.turn = 3;
+		ac.update(false);
+		ac.players[0].wood = 15;
+		ac.b_fences.doClick();
+		ac.b[farmX-1][farmY].doClick();
+		ac.b[farmX+1][farmY].doClick();
+		ac.b[farmX][farmY+1].doClick();
+		ac.b[farmX][farmY-1].doClick();
+		
+		if (orthAdj)
+			farmY+=2;
+		else farmY+=4;
+		
+		ac.b[farmX-1][farmY].doClick();
+		ac.b[farmX+1][farmY].doClick();
+		ac.b[farmX][farmY+1].doClick();
+		ac.b[farmX][farmY-1].doClick();
+		ac.b_finish.doClick();
+		return ac.farm[0][farmX][farmY].getType();
+	}
+	
+	// r-010
+	public char playerBuildField() {
+		AgricolaController ac = new AgricolaController(1);
+		ac.b_field.doClick();
+		ac.b[farmX][farmY].doClick();
+		ac.b_wood.doClick();
+		
+		if (orthAdj)
+			farmY+=2;
+		else farmY+=4;
+		ac.b_field.doClick();
+		ac.b[farmX][farmY].doClick();
+		return ac.farm[0][farmX][farmY].getType();
+	}
+	
+	// r-011
+	//@TODO last
+	
+	// r-012
+	public char playerBuildRoomCheck() {
+		AgricolaController ac = new AgricolaController(1);
+
+		ac.players[0].wood = playerWood;
+		ac.players[0].reed = playerReed;
+		ac.update(false);
+		ac.b_room.doClick();
+
+		ac.b[farmX][farmY].doClick();
+		return ac.farm[0][farmX][farmY].getType();
+	}
+	// r-012
+	public int getFirstPlayerWood() {
+		AgricolaController ac = new AgricolaController(1);
+
+		ac.players[0].wood = playerWood;
+		ac.players[0].reed = playerReed;
+		ac.update(false);
+		ac.b_room.doClick();
+
+		ac.b[farmX][farmY].doClick();
+		return ac.players[0].getWood();
+	}
+	// r-012
+	public int getFirstPlayerReed() {
+		AgricolaController ac = new AgricolaController(1);
+
+		ac.players[0].wood = playerWood;
+		ac.players[0].reed = playerReed;
+		ac.update(false);
+		ac.b_room.doClick();
+
+		ac.b[farmX][farmY].doClick();
+		return ac.players[0].getReed();
+	}
+	
+	// r-013
+	public char playerBuildStableCheck() {
+		AgricolaController ac = new AgricolaController(1);
+
+		ac.players[0].wood = playerWood;
+		ac.update(false);
+		if (stableCost == 1)
+			ac.b_stable.doClick();
+		else ac.b_room.doClick();
+
+		ac.b[farmX][farmY].doClick();
+		return ac.farm[0][farmX][farmY].getType();
+	}
+	// r-013
+	public int playerBuildStableCheckRemainingWood() {
+		AgricolaController ac = new AgricolaController(1);
+
+		ac.players[0].wood = playerWood;
+		ac.update(false);
+		if (stableCost == 1)
+			ac.b_stable.doClick();
+		else ac.b_room.doClick();
+
+		ac.b[farmX][farmY].doClick();
+		return ac.players[0].getWood();
+	}
+	
+	// r-014
+	public int playerBuilFiveStables() {
+		int stables = 0;
+		AgricolaController ac = new AgricolaController(1);
+
+		ac.players[0].wood = 10;
+		ac.update(false);
+		ac.b_stable.doClick();
+
+		ac.b[farmX][farmY].doClick();
+		ac.wStable = false;
+		ac.update(false);
+		ac.b_stable.doClick();
+		ac.b[farmX+2][farmY].doClick();
+		ac.wStable = false;
+		ac.update(false);
+		ac.b_stable.doClick();
+		ac.b[farmX-2][farmY].doClick();
+		ac.wStable = false;
+		ac.update(false);
+		ac.b_stable.doClick();
+		ac.b[farmX][farmY-2].doClick();
+		ac.wStable = false;
+		ac.update(false);
+		ac.b_stable.doClick();
+		ac.b[farmX][farmY+2].doClick();
+		for (int row = 0; row < 7; row++) {
+			for (int col = 0; col < 11; col++) {
+				if (ac.farm[0][row][col].getType() == 's')
+					stables++;
+			}
+		}
+		return stables;
+	}
+	
+	// r-015
+	public int playerBuilSixteenFences() {
+		int fences = 0;
+		AgricolaController ac = new AgricolaController(1);
+
+		ac.players[0].wood = 17;
+		ac.update(false);
+		ac.b_fences.doClick();
+		ac.view.getButtons()[1][2].doClick();
+		ac.view.getButtons()[1][4].doClick();
+		ac.view.getButtons()[1][6].doClick();
+		ac.view.getButtons()[0][3].doClick();
+		ac.view.getButtons()[0][5].doClick();
+		ac.view.getButtons()[2][3].doClick();
+		ac.view.getButtons()[2][5].doClick();
+		ac.view.getButtons()[0][7].doClick();
+		ac.view.getButtons()[2][7].doClick();
+		ac.view.getButtons()[1][8].doClick();
+		ac.view.getButtons()[0][9].doClick();
+		ac.view.getButtons()[2][9].doClick();
+		ac.view.getButtons()[1][10].doClick();
+		ac.view.getButtons()[3][4].doClick();
+		ac.view.getButtons()[3][6].doClick();
+		ac.view.getButtons()[3][8].doClick();
+
+		for (int row = 0; row < 7; row++) {
+			for (int col = 0; col < 11; col++) {
+				if (ac.farm[0][row][col].getType() == 'x')
+					fences++;
+			}
+		}
+		return fences;
+	}
 }
